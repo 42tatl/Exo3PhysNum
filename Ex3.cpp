c8581768682765ebea1428166e481feb722ab3b1
lilimle <lili.mouelle@epfl.ch>
2025-03-19 17:49:09 +0100

    j'ai essaye de faire le cpp mais non

---
+++ Ex3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "ConfigFile.h" // Il contient les methodes pour lire inputs et ecrire outputs 
+#include <valarray>
+#include <cmath> 
+#include <numeric>
+using namespace std;
+
+class Exercice3
+{
+
+private:
+    double t, dt, tfin;
+    double a, ms, mj;
+    
+
+
+
+};
