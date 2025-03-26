e029a1e314d3b771dca1c1c58bd481c663a7f470
lilimle <lili.mouelle@epfl.ch>
2025-03-26 17:59:15 +0100

    bref

--- question_a.py
+++ question_a.py
@@ -2,6 +2,7 @@ import numpy as np
 import subprocess
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
+from matplotlib.lines import Line2D
 import os
 import functions as fct
 
@@ -16,7 +17,7 @@ params = fct.read_in_file(input_filename)
 
 tFin, m1, m2, x0, v0x, v0y, a, tol = fct.get_params(params)
 
-#outputs_nsteps = fct.run_param_sweep(executable, input_filename, "nsteps", [8000,10000,90000], {"adapt": 0, "tol": 30})
+outputs_nsteps = fct.run_param_sweep(executable, input_filename, "nsteps", [8000,10000,90000], {"adapt": 0, "tol": 30})
 outputs_tol = fct.run_param_sweep(executable, input_filename, "tol", [1,0.1,0.01], {"adapt": True, "nsteps": 1000})
 
 
@@ -43,7 +44,7 @@ def plot_energy(t, energy, label=""):
     plt.ylabel("Energie per mass [J/kg]")
     plt.grid(True)
 
-'''
+
 #Fixed time step
 for output in outputs_nsteps:
     t, x, y, vx, vy, energy = read_output_file(output)
@@ -52,7 +53,19 @@ for output in outputs_nsteps:
     plot_trajectory(x, y, label=n_label)
     ax = plt.gca()  # get current axis
     soleil = Circle((0, 0), radius=0.05e12, color='yellow', zorder=10)
+    pos_init = Circle((x[0], y[0]), radius=0.05e12, color='red', zorder=10)
+    pos_final = Circle((x[-1], y[-1]), radius=0.05e12, color='blue', zorder=10)
     ax.add_patch(soleil)
+    ax.add_patch(pos_init)
+    ax.add_patch(pos_final)
+    legend_elements = [
+    Line2D([0], [0], marker='o', color='yellow', label='Sun', markersize=10, linestyle='None'),
+    Line2D([0], [0], marker='o', color='red', label='Initial position', markersize=10, linestyle='None'),
+    Line2D([0], [0], marker='o', color='blue', label='Final position', markersize=10, linestyle='None'),
+    Line2D([], [], color='black', label=n_label)
+]
+
+    plt.legend(handles=legend_elements)
     plt.legend()
     plt.axis("equal")
     plt.savefig(f"traja_{label}.pdf")
@@ -67,7 +80,6 @@ for output in outputs_nsteps:
     plt.legend()
     plt.savefig(f"energiea_{label}.pdf")
     plt.show()
-'''
 
 
 
@@ -79,8 +91,19 @@ for output in outputs_tol:
     plot_trajectory(x, y, label=eps_label)
     ax = plt.gca()  # get current axis
     soleil = Circle((0, 0), radius=0.05e12, color='yellow', zorder=10)
+    pos_init = Circle((x[0], y[0]), radius=0.05e12, color='red', zorder=10)
+    pos_final = Circle((x[-1], y[-1]), radius=0.05e12, color='blue', zorder=10)
     ax.add_patch(soleil)
-    plt.legend()
+    ax.add_patch(pos_init)
+    ax.add_patch(pos_final)
+    legend_elements = [
+    Line2D([0], [0], marker='o', color='yellow', label='Sun', markersize=10, linestyle='None'),
+    Line2D([0], [0], marker='o', color='red', label='Initial position', markersize=10, linestyle='None'),
+    Line2D([0], [0], marker='o', color='blue', label='Final position', markersize=10, linestyle='None'),
+    Line2D([], [], color='black', label=eps_label)
+]
+
+    plt.legend(handles=legend_elements)
     plt.axis("equal")
     plt.savefig(f"traja_adapt_{label}.pdf")
     plt.show()
